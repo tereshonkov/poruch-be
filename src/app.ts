54d6d49a3bb3150f3c1d4a6e52fc54eb55cd7937
tereshonkov <tereshonkov.dima@gmail.com>
2025-08-26 14:43:02 -0700

    fix: add cookie-parser middleware for handling cookies in the application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { userRouter } from './routes/userRoutes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://poruch.click"],
