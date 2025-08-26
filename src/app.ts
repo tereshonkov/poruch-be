f921575b4600bb8777e78bf6e4e8cbbaff1ac124
tereshonkov <tereshonkov.dima@gmail.com>
2025-08-26 16:24:43 +0300

    fix: update import path for userRouter to remove file extension

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { userRouter } from './routes/userRoutes.js';
+import { userRouter } from './routes/userRoutes';
 
 const app = express();
 
