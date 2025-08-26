ec950371ef205d748dbfd1315e5a2b423e6c18f4
tereshonkov <tereshonkov.dima@gmail.com>
2025-08-26 14:28:36 -0700

    fix: remove trailing slash from CORS origin URL for consistency

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://poruch.click/"],
+    origin: ["http://localhost:5173", "https://poruch.click"],
     credentials: true,
   })
 );
