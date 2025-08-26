8888bdcb656c28fb6cd4b4b1fc39e7f62210932a
tereshonkov <tereshonkov.dima@gmail.com>
2025-08-26 14:36:40 -0700

    fix: allow token retrieval from cookies in authentication middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -8,7 +8,7 @@ export default function authMiddleware(req: Request, res: Response, next: NextFu
     }
 
     try {
-        const token = req.headers.authorization?.split(" ")[1];
+        const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
         if (!token) {
             return res.status(401).json({ message: "Користувач не авторизований" });
         }
