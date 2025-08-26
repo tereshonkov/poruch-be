7d1251f69adebf2e0738b0ef59fe2b319d4feabd
tereshonkov <tereshonkov.dima@gmail.com>
2025-08-26 16:13:32 +0300

    fix: set default PORT value to 4000 if not defined in environment

--- server.ts
+++ server.ts
@@ -2,7 +2,8 @@ import app from './src/app.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
