f1540323f1cd5a051d955627345b1d866276561d
tereshonkov <tereshonkov.dima@gmail.com>
2025-08-27 11:07:44 +0300

    feat: add request router to the application for handling requests

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { userRouter } from './routes/userRoutes';
 import cookieParser from 'cookie-parser';
+import requestRouter from './routes/requestRoutes';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(
 );
 app.use(express.json());
 app.use(userRouter);
+app.use(requestRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is alive!");
